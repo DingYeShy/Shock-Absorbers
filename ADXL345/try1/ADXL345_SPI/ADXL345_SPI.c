a143d44d86f3d2f73816fc8dbc1df22be0b2ac5e
DingYeShy <39908616+DingYeShy@users.noreply.github.com>
2018-07-18 22:21:45 +0800

    SPItest0718

--- ADXL345/try1/ADXL345_SPI/ADXL345_SPI.c
+++ ADXL345/try1/ADXL345_SPI/ADXL345_SPI.c
@@ -20,83 +20,92 @@
 #define SCKPU _papu7
 #define CSPU  _pbpu5
 
-#define PAS2 _pas2	//pas 5 SDO
-#define PAS3 _pas3	//pas 6 7 SDI SCL
-#define PBS2 _pbs2	//pbs5 scs
-
 void Delay100us(short);	
 void _SPI_CS(unsigned short);
 void _SPI_SCL(unsigned short);
-char ADXL345_SPI_Read(char);
-void ADXL345_SPI_Write(char ,char);
+unsigned char ADXL345_SPI_Read(unsigned char);
+void ADXL345_SPI_Write(unsigned char,unsigned char);
 void LCD_Cmd(unsigned char data);
 void LCD_Senddata(unsigned char data);
 void LCD_Reset(void);
 void PinSet(void);
 void PrintLCD(unsigned char);
+
 int RawDataX[2],RawDataY[2],RawDataZ[2];
 int* ptrRawData[] =
 {&RawDataX[0],&RawDataX[1],&RawDataY[0],&RawDataY[1],&RawDataZ[0],&RawDataZ[1]};
 int AccX,AccY,AccZ;
 int* ptrAcc[] = {&AccX,&AccY,&AccZ};
-char deviceID;
+unsigned int deviceID;
 short a,b,i;
-char DATAX0 = 0x32;
-char DATAX1 = 0x33;
-char DATAY0 = 0x34;
-char DATAY1 = 0x35;
-char DATAZ0 = 0x36;
-char DATAZ1 = 0x37;
+unsigned int DATAX0;
+unsigned int DATAX1;
+unsigned int DATAY0;
+unsigned int DATAY1;
+unsigned int DATAZ0;
+unsigned int DATAZ1;
 
 void main()
 {
 	PinSet();	//腳位設定
-	Delay100us(200);
+	Delay100us(20);
 	ADXL345_SPI_Write(0x31,0x0B); //設定測量範圍&資料格式
 	ADXL345_SPI_Write(0x2D,0x08); //開始測量
-	//ADXL345_SPI_Write(0x2c, 0x0F);
+	//ADXL345_SPI_Write(0x2c,0x0F);
 	
 	LCD_Reset();		//LCD重置			
-	LCD_Cmd(0xc0);  	//LCD第一行顯示
   
 	while(1)
 	{
-		for(i=5;i>=0;i--)
+		/*for(i=5;i>=0;i--)
 		{
-			*ptrRawData[i] = ADXL345_SPI_Read(i+50);
+			*ptrRawData[i] = (int)ADXL345_SPI_Read(i+50);
 		}
-		/*for(i=0;i<3;i++)
+		for(i=0;i<3;i++)
 		{
 			*ptrAcc[i] = ((*ptrRawData[1+i*2]<<8)&0xFF00) | (*ptrRawData[0+i*2]&0xFF);
 			*ptrAcc[i] = *ptrAcc[i] * 4;
-		}*/
-		/*AccX = ((RawDataX[1]<<8)&0xFF00)|(RawDataX[0]&0xFF);
+		}
+		AccX = ((RawDataX[1]<<8)&0xFF00)|(RawDataX[0]&0xFF);
 		AccX = AccX*4;
 		AccY = ((RawDataY[1]<<8)&0xFF00)|(RawDataY[0]&0xFF);
 		AccY = AccY*4;
 		AccZ = ((RawDataZ[1]<<8)&0xFF00)|(RawDataZ[0]&0xFF);
 		AccZ = AccZ*4;*/
 		
-		/*AccX = (((int)ADXL345_SPI_Read(DATAX1)&0xFF00)) | ((int)ADXL345_SPI_Read(DATAX0)&0xFF);
-		AccY = (((int)ADXL345_SPI_Read(DATAY1)&0xFF00)) | ((int)ADXL345_SPI_Read(DATAY0)&0xFF);
-		AccZ = (((int)ADXL345_SPI_Read(DATAZ1)&0xFF00)) | ((int)ADXL345_SPI_Read(DATAZ0)&0xFF);*/
+		AccX = (ADXL345_SPI_Read(0x33)&0xFF00) | (ADXL345_SPI_Read(0x32)&0xFF);
+		AccX = AccX * 4;
+		AccY = (ADXL345_SPI_Read(0x35)&0xFF00) | (ADXL345_SPI_Read(0x34)&0xFF);
+		AccY = AccY * 4;
+		AccZ = (ADXL345_SPI_Read(0x37)&0xFF00) | (ADXL345_SPI_Read(0x36)&0xFF);
+		AccZ = AccZ * 4;
 		
 		/*AccX = ((int)ADXL345_SPI_Read(DATAX1)<<8)|(int)ADXL345_SPI_Read(DATAX0);
 		AccY = ((int)ADXL345_SPI_Read(DATAY1)<<8)|(int)ADXL345_SPI_Read(DATAY0);
 		AccZ = ((int)ADXL345_SPI_Read(DATAZ1)<<8)|(int)ADXL345_SPI_Read(DATAZ0);*/
-		
+		/*Delay100us(200);
 		LCD_Cmd(0x80);  //從第一行第0位置開始顯示   C0為第二行
 	 	PrintLCD(AccX);
-	  	
+	  	Delay100us(200);
 	  	LCD_Cmd(0x86);	//從第一行第5位置開始顯示
 	  	PrintLCD(AccY);
-		
+		Delay100us(200);
 		LCD_Cmd(0x8d);	//從第一行第5位置開始顯示
-	  	PrintLCD(AccZ);
+	  	PrintLCD(AccZ);*/
 
-		deviceID = ADXL345_SPI_Read(0x44);
+		deviceID = (unsigned int)ADXL345_SPI_Read(0x00);
 		LCD_Cmd(0xc0);	//從第二行第0位置開始顯示
 	  	PrintLCD(deviceID);
+	  	
+	  	/*DATAZ0 = (int)ADXL345_SPI_Read(0x36);
+		LCD_Cmd(0xc6);	//從第二行第0位置開始顯示
+	  	PrintLCD(DATAZ0);
+	  	
+	  	DATAZ1 = (int)ADXL345_SPI_Read(0x37);
+		LCD_Cmd(0xcd);	//從第二行第0位置開始顯示
+	  	PrintLCD(DATAZ1);*/
+	  	
+	  	Delay100us(100);
 	}
 }
 //////////////////////////////
@@ -109,32 +118,21 @@ void PinSet(void)
 	_cp1c = 0x00;
 	_scomen = 0;
 	_smod = 0xe0;//4MHz
-	/*_sdis0=0;
-	_sdis1=0;
-	_simen=1;  
-	_simc0=0x42;
-	_simc2=0x24;*/
 	
-	//PAS2=0x10;  //設定各個腳位功能
-	//PAS3=0x22;  
-	//PBS2=0x10;
-
-	
-	_ifs4=0;
-	
-	_pac5=0; 	_pac6=1; 	_pac7=0;	CSC=0;//設定I/O
-	_phc0 =0;   _phc1=0;    _phc2=0; //設置PH0、PH1、PH2為輸出   
-	SCK=0;		CS=1;		_pgc = 0x00;   
+	SDOC=0; 	SDIC=1; 	SCKC=0;	    CSC=0;//設定I/O
+	_phc0 =0;   _phc1=0;    _phc2=0;    //設置PH0、PH1、PH2為輸出 
+	SCK=0;	CS=1;     //腳位初始狀態   
+	_pgc = 0x00;   
 }
 
 
 //////////////////////////////
-///SPI讀取涵式
+/////////SPI讀取涵式//////////
 //////////////////////////////
-char ADXL345_SPI_Read(char Address)
+unsigned char ADXL345_SPI_Read(unsigned char Address)
 {
-  char ReadData=0;
-  char tempSDO;
+  unsigned char ReadData=0;
+  unsigned char tempSDO;
   _SPI_CS(0);
   a=7;
   do{
@@ -144,9 +142,8 @@ char ADXL345_SPI_Read(char Address)
     _SPI_SCL(1);
     SDO = 0x1 & ((0x80 | Address) >> a);
     _SPI_SCL(0);
-    a--;
-  }while(a!=0);
- 
+    --a;
+  }while(a!=0); 
   //===========================
   _SPI_SCL(1);
   //===========================
@@ -157,8 +154,7 @@ char ADXL345_SPI_Read(char Address)
     _SPI_SCL(1);
     tempSDO = SDI; // Read bit
     ReadData |= tempSDO << a;
-    a--;
-  }while(a>=0);
+  }while(a--!=0);
  
   _SPI_CS(1);
   return ReadData & 0xFF;
@@ -178,7 +174,7 @@ void PrintLCD(unsigned char data)
 ///Delay100us涵式
 //////////////////////////////
 void Delay100us(short del)						//延遲del*200指令週期
-{	unsigned short k,l;									//@fSYS=8MH,延遲del*100us
+{	unsigned short k,l;							//@fSYS=8MH,延遲del*100us
 	for(k=0;k<del;k++)
 		for(l=0;l<=25;l++)GCC_NOP();
 }	
@@ -189,7 +185,7 @@ void Delay100us(short del)						//延遲del*200指令週期
 void _SPI_CS(unsigned short isSelect)
 {
     CS=isSelect;
-    _nop();
+    GCC_NOP();
 }
 
 //////////////////////////////
@@ -198,31 +194,32 @@ void _SPI_CS(unsigned short isSelect)
 void _SPI_SCL(unsigned short bLevel)
 {
     SCK=bLevel;
-    _nop();
+    GCC_NOP();
 }
 
 //////////////////////////////
 ///SPI寫入涵式
 //////////////////////////////
-void ADXL345_SPI_Write(char Address, char WriteData)
+void ADXL345_SPI_Write(unsigned char Address,unsigned char WriteData)
 {
   _SPI_CS(0);
-  for(b=7;b>=0;b--)
-  {
+  b=7;
+  do{
     // F-Edge
     _SPI_SCL(1);
-    SDI = 0x1 & ((0x7F & Address) >> b);
+    SDO = 0x1 & ((0x7F & Address) >> b);
     _SPI_SCL(0);
-  }
+    --b;
+  }while(b!=0);
   
-  for(b=7;b>=0;b--)
-  {
+  b=7;
+  do{
     // F-Edge
     _SPI_SCL(1);
-    SDI = 0x1 & ((WriteData) >> b);
+    SDO = 0x1 & ((WriteData) >> b);
     _SPI_SCL(0);
-  }
-
+    --b;
+  }while(b!=0);
   _SPI_CS(1);
 }
 
